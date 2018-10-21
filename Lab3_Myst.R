4ef950c72a8c1b17c3ab745e3fb667be563918df
Alfonso Aguilar <tripleaguilar@hotmail.com>
2018-10-21 15:36:17 -0500

    Lab 3
    Estrategia de trading promedio movil ponderado

--- Lab3_Myst.R
+++ Lab3_Myst.R
@@ -42,7 +42,7 @@ OA_Ta <- "America/Mexico_City"
 # Instrumento
 OA_In <- "EUR_USD"
 # Granularidad o periodicidad de los precios H4 = Cada 4 horas
-OA_Pr <- "H4"
+OA_Pr <- "D"
 # Multiplicador de precios para convertir a PIPS
 MultPip_MT1 <- 10000
 
@@ -53,5 +53,163 @@ Precios_Oanda <- HisPrices(AccountType = OA_At, Granularity = OA_Pr,
 # calcular ganancias va a ser el precio de cierre menos el precio de apertura por 
 # los 10,000 unidadeas que compraremos (para que el valor del pip sea 1$) por el 
 # valor del pip 
+
+######################
+# Funcion de aceptacion de promedio movil
+Regla0_R <- -.045  # Considerar una oportunidad de compra en un rendimiento de -6% o menor.
+BorS <- function(Precio,npm,pond,t){
+  pm   <- c()
+  if(sum(pond*Precio[(t-npm):(t-1)])<Precio[t]){
+    return(1)
+  }
+  else{
+    return(0)
+  }
+}
+######################
+
+npm <- 21
+pondt <- runif(npm, min=0, max=1)
+pond <- pondt/sum(pondt)
+Regla1_I <- .2   # Porcentaje de capital para comprar titulos para posicion Inicial.
+Regla2_P <- .15   # Se utiliza el P% del L capital restante en cada compra.
+Regla3_W <- Precios_Oanda # Se realiza la misma estrategia para todos los activos en el portafolio.
+Regla4_C <- 0.0025 # Comisiones pagadas por compra.
+Regla5_K <- 100000 # Capital Inicial.
+
+
+#Creacion del data frame
+Historico   <- c()
+Historico <- data.frame("Date" = row.names(Precios_Oanda),
+                             "Precio" = Precios_Oanda$Close, 
+                             "R_Precio" = 0, 
+                             "R_Activo" = 0,
+                             "R_Cuenta" = 0, 
+                             "Capital" = 0,"Flotante" = 0, "Balance" = 0, "Titulos" = 0,
+                             "Titulos_a" = 0,
+                             "Operacion" = NA, "Comisiones" = 0,"Comisiones_a" = 0, "Mensaje" = NA)
+
+
+# -- El rendimiento de capital en el tiempo 1 es 0
+Historico$R_Cuenta[1] <- 0
+
+# -- Calcular R_Precio
+Historico$R_Precio <- round(c(0, diff(log(Historico$Precio))),4)
+
+##inicializacion de la cartera
+Historico$Titulos[npm] <- (Regla5_K*Regla1_I)%/%Historico$Precio[npm]
+
+Historico$Titulos_a[npm]<-Historico$Titulos[npm]
+
+# -- Se calculan comisiones iniciales
+Historico$Comisiones[npm] <- Historico$Titulos[npm]*Historico$Precio[npm]*Regla4_C
+Historico$Comisiones_a[npm] <- Historico$Comisiones[npm]
+
+# --- Calcular el valor flotante de la posicion
+Historico$Flotante[npm] <- Historico$Titulos_a[npm]*Historico$Precio[npm]
+
+# -- Todo remanente se dejar? registrado en la cuenta de efectivo.
+Historico$Capital[npm] <- Regla5_K-Historico$Flotante[npm]-Historico$Comisiones[npm]
+
+# -- Calcular el Balance
+Historico$Balance[npm] <- Historico$Flotante[npm]+Historico$Capital[npm]
+
+# -- Iniciamos con una postura de mantener.
+Historico$Operacion[npm] <- "Posicion Inicial"
+
+
+# -- Mensaje inicial
+Historico$Mensaje[npm] <- "Inicializacion de cartera"
+
+
+for(i in (npm+1):(length(Historico$Date))){
+  Historico$R_Activo[i] <- round((Historico$Precio[i]/Historico$Precio[1])-1,2)
+  if(BorS(Historico$Precio,npm,pond,i)){ #se activa una se??al
+    Historico$Capital[i] <- Historico$Capital[i-1]
+    if(Historico$Capital[i] > 0){ # Si hay capital
+      if(Historico$Capital[i]*Regla2_P > Historico$Precio[i]*Historico$Capital[i]*Regla2_P){ # Si Capital minimo
+        Historico$Operacion[i] <- "Compra"
+        Historico$Titulos[i]   <- (Historico$Capital[i]*Regla2_P)%/%Historico$Precio[i]
+        compra <- Historico$Precio[i]*Historico$Titulos[i]  
+        Historico$Comisiones[i] <- compra*Regla4_C
+        Historico$Comisiones_a[i] <- Historico$Comisiones_a[i-1]+Historico$Comisiones[i]
+        Historico$Titulos_a[i] <- Historico$Titulos[i-1]+Historico$Titulos[i]
+        Historico$Capital[i]<-Historico$Capital[i-1]-compra-Historico$Comisiones[i]
+        Historico$Titulos_a[i]<-Historico$Titulos[i]+Historico$Titulos_a[i-1]
+        Historico$Flotante[i] <- Historico$Titulos_a[i]*Historico$Precio[i]
+        Historico$Balance[i] <- Historico$Capital[i]+Historico$Flotante[i]
+        Historico$Mensaje[i] <- "Se hizo una compra"
+        Historico$R_Cuenta[i]<-Historico$Balance[i]/Regla5_K-1
+      }
+      else{
+        Historico$Operacion[i] <- "Compra"
+        Historico$Capital[i]<-Historico$Capital[i-1]
+        Historico$Titulos[i] <-0
+        Historico$Comisiones[i] <-0
+        Historico$Comisiones_a[i] <- Historico$Comisiones_a[i-1]
+        Historico$Titulos_a[i]<-Historico$Titulos[i]+Historico$Titulos_a[i-1]
+        Historico$Flotante[i] <- Historico$Titulos_a[i]*Historico$Precio[i]
+        Historico$Balance[i] <- Historico$Capital[i]+Historico$Flotante[i]
+        Historico$Mensaje[i] <- "Capital insuficiente para cubrir la regla"
+        Historico$R_Cuenta[i]<-Historico$Balance[i]/Regla5_K-1
+      }
+    }
+    else { # No hubo capital
+      Historico$Operacion[i] <- "Compra"
+      Historico$Capital[i]<-Historico$Capital[i-1]
+      Historico$Titulos[i] <-0
+      Historico$Comisiones[i] <-0
+      Historico$Comisiones_a[i] <- Historico$Comisiones_a[i-1]
+      Historico$Titulos_a[i]<-Historico$Titulos[i]+Historico$Titulos_a[i-1]
+      Historico$Flotante[i] <- Historico$Titulos_a[i]*Historico$Precio[i]
+      Historico$Balance[i] <- Historico$Capital[i]+Historico$Flotante[i]
+      Historico$Mensaje[i] <- "Capital insuficiente"
+      Historico$R_Cuenta[i]<-Historico$Balance[i]/Regla5_K-1
+    }
+  }
+  else{
+    if(Historico$Titulos_a[i-1] > 0){ #Si hay acciones para vender
+      Historico$Operacion[i] <- "Venta"
+      Historico$Titulos[i] <-Historico$Titulos_a[i-1]
+      venta <- Historico$Precio[i]*Historico$Titulos[i]
+      Historico$Comisiones[i] <- venta*Regla4_C
+      Historico$Comisiones_a[i] <- Historico$Comisiones_a[i-1]+Historico$Comisiones[i]
+      Historico$Capital[i]<-Historico$Capital[i-1]-venta-Historico$Comisiones[i]
+      Historico$Titulos_a[i] <-0
+      Historico$Flotante[i] <- Historico$Titulos_a[i]*Historico$Precio[i]
+      Historico$Balance[i] <- Historico$Capital[i]+Historico$Flotante[i]
+      Historico$Mensaje[i] <- "Se hizo una venta"
+      Historico$R_Cuenta[i]<-Historico$Balance[i]/Regla5_K-1
+    }
+    else{
+      Historico$Operacion[i] <- "Venta"
+      Historico$Mensaje[i] <- "Activos insuficientes"
+      Historico$Capital[i]<-Historico$Capital[i-1]
+      Historico$Titulos[i] <-0
+      Historico$Titulos_a[i]<-Historico$Titulos[i]+Historico$Titulos_a[i-1]
+      Historico$Comisiones[i] <-0
+      Historico$Comisiones_a[i] <- Historico$Comisiones_a[i-1]+Historico$Comisiones[i]
+      
+      Historico$Flotante[i] <- Historico$Titulos_a[i]*Historico$Precio[i]
+      Historico$Balance[i] <- Historico$Capital[i]+Historico$Flotante[i]
+      Historico$R_Cuenta[i]<-Historico$Balance[i]/Regla5_K-1
+    }
+  }
+}
+
+pm<-c()
+for(i in (npm+1):(length(Historico$Date))){
+  pm[i-npm]<- sum(pond*Historico$Precio[(i-npm):(i-1)]) 
+}
+
+
+
+
+
+
+
+
+
+
 
 
